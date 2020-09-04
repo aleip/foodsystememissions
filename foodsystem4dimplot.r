db4a5d2f12f8030c593ab0bac24d34476f1aed56
Adrian Leip <adrian@leip.net>
2020-09-04 17:12:25 +0200

    Improving multi-dim intensity plot

--- foodsystem4dimplot.r
+++ foodsystem4dimplot.r
@@ -43,12 +43,13 @@ cost2use <- cost2use[Region=="SPP", Region:="ESP"] #Spain
 cost2use <- cost2use[Region=="UKP", Region:="GBR"] #United Kingdom
 
 load(paste0(edgar_folder, "data/202005/edgar_food_20200617.rdata"))
+
 edgarex <- dcast.data.table(edgarfood[variable==2015 & compartment != "", 
                                       .(value = sum(value, na.rm = TRUE)), 
                                       by = .(countries = Country_code_A3, compartment)], 
                                       countries ~ compartment, value.var = "value", fill = 0)
-#edgarcost <- merge(edgar[, .(countries, foodtCO2cap, land2landenergy, sqrtktPcap)], 
-#                   cost2use, by.x = "countries", by.y = "Region", all = TRUE)
+edgarex <- merge(edgarex, countrytable[, .(countries = Country_code_A3, region=C_group_IM24_sh, dev=dev_country)])
+
 edgarcost <- merge(edgarex, cost2use, by.x = "countries", by.y = "Region", all = TRUE)
 
 
@@ -111,67 +112,132 @@ gdb4plot <- gdb4plot[! is.na(ISOcode)]
 gdb4plot <- gdb4plot[ISOcode != "", -"location", with=FALSE]
 
 edgarcostgdb <- merge(edgarcost, gdb4plot[, .(countries=ISOcode, val_all, val_BMI, val_child, val_dietary)], by="countries", all.x = TRUE)
-edgarcostgdb <- edgarcostgdb[, 
-                             dominance := ifelse(val_child/(val_child+val_BMI+val_dietary)>0.62, 1, 
-                                                 ifelse(val_BMI/(val_child+val_BMI+val_dietary)>0.23,2, 3))]
 
-# Calculate GHG metrics
-edgarcostgdb[, `:=` (foodtCO2cap = (Energy+Industry+Landbased+Waste)/pop,
-                     land2landenergy = (Landbased / (Energy+Landbased)),
-                     # Find a suitable transformation for bubble size 
-                     # based on foodtCO2cap
-                     sqrtktPcap = (foodtCO2cap/1000)^0.5)]
+edgarcostgdb_abs <- edgarcostgdb[, .(countries, region, dev, 
+                                     tot=Energy+Industry+Landbased+Waste,
+                                     land=Landbased,
+                                     energy=Energy+Industry,
+                                     pop, cost=pop*costPcap,
+                                     val_all_abs=pop*val_all,
+                                     val_BMI_abs=pop*val_BMI,
+                                     val_child_abs=pop*val_child,
+                                     val_dietary_abs=pop*val_dietary)]
+cols2sum <- c("tot", "land", "energy", "pop", "cost", "val_all_abs", "val_BMI_abs", "val_child_abs", "val_dietary_abs")
+e1 <- edgarcostgdb_abs[, lapply(.SD, sum, na.rm=TRUE), by=.(countries), .SDcols=cols2sum]
+e2 <- edgarcostgdb_abs[, lapply(.SD, sum, na.rm=TRUE), by=.(region), .SDcols=cols2sum]
+e3 <- edgarcostgdb_abs[, lapply(.SD, sum, na.rm=TRUE), by=.(dev), .SDcols=cols2sum]
+e1$scale <- "country"; setnames(e1, "countries", "reg")
+e2$scale <- "region"; setnames(e2, "region", "reg")
+e3$scale <- "development"; setnames(e3, "dev", "reg")
 
+e4plot <- rbind(e1, e2, e3)
 
-hist(edgarcostgdb$dominance)
-hist(edgarcostgdb$foodtCO2cap)
+# Convert back to per capita data
+e4plot <- e4plot[, .(
+  scale, reg, tot, land, energy, pop,
+
+  # Convert back to per capita data
+  costPcap = cost/pop,
+  val_all = val_all_abs/pop,
+  val_BMI = val_BMI_abs/pop,
+  val_child = val_child_abs/pop,
+  val_dietary = val_dietary_abs/pop,
+  
+  # Calculate GHG metrics
+  foodtCO2cap = tot/pop,
+  enind2landenind = energy / (land+energy),
+  
+  # Find a suitable transformation for bubble size
+  # based on foodtCO2cap
+  sqrtktPcap = ((tot/pop) /1000)^0.5
+  
+)]
+
+e4plot <- e4plot[, 
+                 dominance := ifelse(val_child/(val_child+val_BMI+val_dietary)>0.62, 1, 
+                                     ifelse(val_BMI/(val_child+val_BMI+val_dietary)>0.23,2, 3))]
+hist(e4plot$dominance)
+hist(e4plot$foodtCO2cap)
 summary(edgarcostgdb)
-edgarcostgdb[, .(emismn = min(foodtCO2cap),
-                 emis05 = quantile(foodtCO2cap, 0.05),
-                 emis50 = quantile(foodtCO2cap, 0.50),
-                 emis95 = quantile(foodtCO2cap, 0.95),
-                 emismx = max(foodtCO2cap),
-                 sharmn = min(land2landenergy),
-                 shar05 = quantile(land2landenergy, 0.05),
-                 shar50 = quantile(land2landenergy, 0.50),
-                 shar95 = quantile(land2landenergy, 0.95),
-                 sharmx = max(land2landenergy),
-                 costmn = min(costPcap),
-                 cost05 = quantile(costPcap, 0.05),
-                 cost50 = quantile(costPcap, 0.50),
-                 cost95 = quantile(costPcap, 0.95),
-                 costmx = max(costPcap)
-                 )]
+e4plot[, .(emismn = min(foodtCO2cap),
+           emis05 = quantile(foodtCO2cap, 0.05),
+           emis50 = quantile(foodtCO2cap, 0.50),
+           emis95 = quantile(foodtCO2cap, 0.95),
+           emismx = max(foodtCO2cap),
+           sharmn = min(land2landenergy),
+           shar05 = quantile(land2landenergy, 0.05),
+           shar50 = quantile(land2landenergy, 0.50),
+           shar95 = quantile(land2landenergy, 0.95),
+           sharmx = max(land2landenergy),
+           costmn = min(costPcap),
+           cost05 = quantile(costPcap, 0.05),
+           cost50 = quantile(costPcap, 0.50),
+           cost95 = quantile(costPcap, 0.95),
+           costmx = max(costPcap)
+)]
 
-plotdata <- edgarcostgdb[sqrtktPcap<100, 
-                         .(countries, 
-                           x=costPcap, 
-                           y=as.numeric(land2landenergy), 
-                           BMI=sqrtktPcap, 
-                           Diet=sqrtktPcap*(val_dietary+val_child)^2/(val_BMI+val_dietary+val_child)^2, 
-                           Child=sqrtktPcap*val_child^2/(val_BMI+val_dietary+val_child)^2
-                         )]
-px <- plotdata[x>5 & x<6]
-px <- plotdata
-px <- px[order(BMI, decreasing=TRUE)]
-px <- as.data.frame(px)
-pxscale <- 5
-#p <- ggplot(x, aes(costPcap, land2landenergy)) + geom_point(color = x$val_all, size  = 5*x$sqrtktPcap)
-p <- ggplot(px, aes(x, y)) 
-#p <- p + geom_point(shape=21, size  = px$ALL, fill="grey", colour="black", stroke=0.2)
-p <- p + geom_point(shape=21, size  = pxscale*px$BMI, fill="blue", stroke=0)
-p <- p + geom_point(shape=21, size  = pxscale*px$Diet, fill="yellow", stroke=0)
-p <- p + geom_point(shape=21, size  = pxscale*px$Child, fill="red", stroke=0)
-p <- p + ggtitle("Food system GHG intensity and relative deaths by all causes attributed food system risk factors") + 
-  xlab("Whole sale cost for food [$ cap-1 day-1]. Source: Springmann et al.") +
-  ylab("Share of GHG emissions from energy")
+do.intensityplot <- function(dt, doprint=FALSE){
+  
+  plotdata <- copy(dt)
+  
+  sel <- unique(plotdata$scale)
+  
+  plotdata <- plotdata[sqrtktPcap<100, 
+                       .(reg, 
+                         x=costPcap, 
+                         y=as.numeric(enind2landenind), 
+                         BMI=sqrtktPcap, 
+                         Diet=sqrtktPcap*(val_dietary+val_child)^2/(val_BMI+val_dietary+val_child)^2, 
+                         Child=sqrtktPcap*val_child^2/(val_BMI+val_dietary+val_child)^2
+                       )]
+  px <- plotdata[x>5 & x<6]
+  px <- plotdata
+  px <- px[order(BMI, decreasing=TRUE)]
+  px <- as.data.frame(px)
+  pxscale <- 7
+  
+  #p <- ggplot(x, aes(costPcap, land2landenergy)) + geom_point(color = x$val_all, size  = 5*x$sqrtktPcap)
+  p <- ggplot(px, aes(x, y)) 
+  #p <- p + geom_point(shape=21, size  = px$ALL, fill="grey", colour="black", stroke=0.2)
+  p <- p + geom_point(shape=21, size  = pxscale*px$BMI, fill="blue", stroke=0)
+  p <- p + geom_point(shape=21, size  = pxscale*px$Diet, fill="yellow", stroke=0)
+  p <- p + geom_point(shape=21, size  = pxscale*px$Child, fill="red", stroke=0)
+  p <- p + ggtitle("Food system GHG intensity and diet-related deaths") + 
+    xlab("Whole sale cost for food [$ cap-1 day-1]. Source: Springmann et al.") +
+    ylab("Share of GHG emissions from energy")
+  
+  # p <- p + geom_text(label=px$reg, 
+  #                  nudge_x = 0.0, 
+  #                  nudge_y=px$BMI/80, 
+  #                  check_overlap = FALSE, # Seems to put it very far away 
+  #                  size = 3)
+  p <- p + geom_text(aes(label=px$reg, vjust=px$BMI*1.5), size=3)
+  #p <- p + geom_scatterpie(aes(x=x, y=y, r=size), data=x, cols=c("BMI", "Dietary", "Child"), color=NA)
+  print(p) 
+  
+  if(doprint) {
+    png(filename = paste0(plot_folder, "ghgintensityplot_", sel, today(), ".png"), width=10000, height=5000, units="px", res=1000)
+    print(p)
+    dev.off()
+  }  
+  
+  return(p)
+}
+
+pcounrty <- do.intensityplot(e4plot[scale=="country"], doprint = TRUE)
+pdev <- do.intensityplot(e4plot[scale=="development"], doprint = TRUE)
+preg <- do.intensityplot(e4plot[scale=="region"], doprint = FALSE)
+
+preg + geom_text(label=e4plot[scale=="region"][1:5]$reg, 
+                 nudge_x = 0.0, 
+                 nudge_y=e4plot[scale=="region"][1:5]$sqrtktPcap + 0.01, 
+                 check_overlap = TRUE, 
+                 size = 3)
 
 
-#p <- p + geom_scatterpie(aes(x=x, y=y, r=size), data=x, cols=c("BMI", "Dietary", "Child"), color=NA)
-p 
+e4plot[scale=="region"][order(costPcap)][, .(reg, tot, costPcap, sqrtktPcap, enind2landenind)]
 
-png(filename = paste0(plot_folder, "ghgintensityplot", today(), ".png"), width=10000, height=5000, units="px", res=1000)
-print(p)
-dev.off()
 
 save(gdb, gdb4plot, edgar, cost2use, edgarcost, file=paste0(plot_folder, "ghgintensityplot", today(), ".rdata"))
+
+
